dd65af25d2fe3e158879fa265f5ef952196bc12f
vvyvi <ewawozny4@gmail.com>
2021-04-12 00:01:36 +0200

    styles

--- src/hooks/Socket.js
+++ src/hooks/Socket.js
@@ -2,8 +2,8 @@ import React, {useState, createContext, useEffect} from 'react'
 import socketIOClient from "socket.io-client";
 import {displayCustom, displayMessage} from "../components/organisms/info";
 
-const ENDPOINT = "http://127.0.0.1:4001";
-const socketClient = socketIOClient(ENDPOINT);
+const ENDPOINT = "http://127.0.0.1:4001"
+const socketClient = socketIOClient(ENDPOINT)
 
 export const SocketContext = createContext()
 
